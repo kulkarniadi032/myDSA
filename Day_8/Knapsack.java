b443bc6b4d6e9deda537293cc9ce9495f16e384c
kulkarniadi032 <78421588+kulkarniadi032@users.noreply.github.com>
2021-07-24 15:02:45 +0530

    Knapsack problem code

--- Day_8/Knapsack.java
+++ Day_8/Knapsack.java
@@ -65,19 +65,20 @@ public class Knapsack {
 
         // for print the Profits array elements
         System.out.println("Profits are: ");
-        float profitArr[] = { 40, 30, 20 };
+        float profitArr[] = { 10, 5, 15, 7, 6, 18, 3 };
         k1.printProfitArray(profitArr);
         System.out.println();
         // for print the weight array elements
         System.out.println("Weights are: ");
-        float weightArr[] = { 20, 10, 5 };
+        float weightArr[] = { 2, 3, 5, 7, 1, 4, 1 };
         k1.printWeightArray(weightArr);
         // for find the p/w value
         System.out.println();
         System.out.println("enter capacity of knapsack: ");
         float capacity = sc.nextFloat();
 
-        float newArr[][] = { { 40, 20, 0 }, { 30, 10, 0 }, { 20, 5, 0 } };
+        float newArr[][] = { { 10, 2, 0 }, { 5, 3, 0 }, { 15, 5, 0 }, { 7, 7, 0 }, { 6, 1, 0 }, { 18, 4, 0 },
+                { 3, 1, 0 } };
         System.out.println("Ratio are");
         k1.ratio(profitArr, weightArr, newArr);
         int col = 3;
@@ -126,11 +127,23 @@ public class Knapsack {
 // test case1 :
 // profit : 40,30,20
 // weight : 20,10,5
+// capacity : 20
+// ans : 60
 
 // test case2:
 // profit : 10,5,15,7,6,18,3
 // weight : 2,3,5,7,1,4,1
+// capacity : 15
+// ans : 54
 
 // test case3 :
 // profit : 60,100,120
 // weight : 10,20,30
+// capacity : 50
+// ans : 240
+
+// test case4 :
+// profit : 60,50,20
+// weight : 20,25,5
+// capacity : 40
+// ans : 110
